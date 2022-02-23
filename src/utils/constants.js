cca92cdb52e104cf313fa5991bf6024a59e7978e
jacknguyen <jack.nguyen@weareday.one>
2022-02-23 23:14:53 +0700

    update for rinkeby

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
 export const MoralisConfigs = {
   // dev
-  serverUrl: "https://mndttulob3te.usemoralis.com:2053/server",
-  appId: "BneLsmybBe0xewJ70WDlVCaMjwEswr3hxyPQO7kd",
+  // serverUrl: "https://mndttulob3te.usemoralis.com:2053/server",
+  // appId: "BneLsmybBe0xewJ70WDlVCaMjwEswr3hxyPQO7kd",
   // stage
-  // serverUrl: "https://6zitu24v62ou.usemoralis.com:2053/server",
-  // appId: "mKu4P0mSPKHy23MV7IzqCdRxZIMbEvcKlbQE56d7",
+  serverUrl: "https://6zitu24v62ou.usemoralis.com:2053/server",
+  appId: "mKu4P0mSPKHy23MV7IzqCdRxZIMbEvcKlbQE56d7",
 };
 
 export const ChainIds = {
@@ -19,10 +19,10 @@ export const NetworkIds = {
   mainnet: "1",
 };
 
-export const chainId = ChainIds.ganache; // Ganache
-// export const chainId = ChainIds.rinkeby; // Rinkeby
+// export const chainId = ChainIds.ganache; // Ganache
+export const chainId = ChainIds.rinkeby; // Rinkeby
 // export const chainId = ChainIds.mainnet; // Mainnet
 
-export const networkId = NetworkIds.ganache; // Ganache
-// export const networkId = NetworkIds.rinkeby; // Rinkeby
+// export const networkId = NetworkIds.ganache; // Ganache
+export const networkId = NetworkIds.rinkeby; // Rinkeby
 // export const networkId = NetworkIds.mainnet; // Mainnet
